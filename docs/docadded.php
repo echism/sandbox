8c6a3fb009efba30cf6b839301731d480a382152
echism <echism@iu.edu>
2018-04-26 08:25:58 -0400

    new file:   docs/docadded.php

---
+++ docs/docadded.php
@@ -0,0 +1,20 @@
+<html>
+
+	<head>
+		<title>Document Entered</title>
+	</head>
+	
+	<body>
+
+	<?php
+	
+	$url = $_POST['URL'];
+	
+	echo $URL;
+	
+	echo date('Y-m-d');
+	
+	
+	?>
+	</body>
+</html>
